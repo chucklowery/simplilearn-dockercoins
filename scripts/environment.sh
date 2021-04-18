760f6938d1c7c5c7a37a9be5cc145df4c7433c92
Charles H. Lowery <chucklowery@gmail.com>
2021-04-18 12:30:52 -0600

    Create environment.sh

---
+++ scripts/environment.sh
@@ -0,0 +1,28 @@
+
+          set -x
+          
+          for service in hasher rng webui worker
+            do
+              docker build --tag chucklowery/simplilearn-dockercoins:latest-${service}-testing ${service}
+            done
+            
+          for service in hasher redis rng
+            do
+              docker network create ${service}
+            done
+          
+          # --volumes XXX:/data will connect /data inside the container with the following folder outside the container: /var/lib/docker/volumes/XXX
+          docker run --detach --name redis --network redis --volume redis:/data redis
+          
+          for service in hasher rng
+            do
+              docker run --detach --name ${service} --network ${service} chucklowery/simplilearn-dockercoins:latest-${service}-testing
+            done
+            
+          docker run --detach --name worker --network redis chucklowery/simplilearn-dockercoins:latest-worker-testing
+          for service in hasher rng
+            do
+              docker network connect ${service} worker
+            done
+          
+          docker run --detach --name webui --network redis chucklowery/simplilearn-dockercoins:latest-webui-testing
